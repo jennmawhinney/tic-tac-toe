ed7e911b855d831fb174ea90113da26184b21416
jennmawhinney <jennnobile@icloud.com>
2016-03-08 10:26:13 +1100

    second commit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+// $(document).ready(function(){
+//
+// var whatsX = [1,5,7];
+// var whatsY = [];
+//
+// var ticTacDough = {
+//
+//     checkX: function(input){
+//       for (var i = 0; i < whatsX.length; i++) {
+//         return[i];
+//       }
+//     }
+//
+// };
+//   $(".box").on('click', function(){
+//     console.log('hello');
+//
+//   });
+//
+//
+// });
+
+// funtion to show which box has been clicked
+$(".box").on('click', function(){
+    console.log('hello');
+
+  });
+// function to show whose turn it is
+//
